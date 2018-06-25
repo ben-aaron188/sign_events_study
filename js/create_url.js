b9b9c1d87da11dc9cf202eff219f2b4702a2bd14
BKleinberg <bennettkleinberg@gmail.com>
2018-06-25 15:04:13 +0200

    preparations exp2

--- js/create_url.js
+++ js/create_url.js
@@ -1,5 +1,5 @@
-// var original_url = 'https://ben1108.typeform.com/to/SiaXax'; //for t
-var original_url = 'https://ben1108.typeform.com/to/dAndxW'; //for d
+var original_url = 'https://ben1108.typeform.com/to/rBHhn4'; //for t
+// var original_url = 'https://ben1108.typeform.com/to/dYgcG9'; //for d
 function get_url(unid_in) {
     var url_string = original_url + '?unid=' + unid_in;
     return url_string;
